fcc3ee1a665a543d3f1ceb624be6997ba1a8c258
Frida_Cera <fridaapodaca541@gmail.com>
2025-03-04 16:51:55 +0000

    enviado

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,71 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MiContenedorAPP());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiContenedorAPP extends StatelessWidget {
+  const MiContenedorAPP({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Frida Sofia Apodaca Cera"),
+          centerTitle: true,
+          titleTextStyle:
+              const TextStyle(color: Color(0xffa3d0fb), fontSize: 25),
+          backgroundColor: const Color(0xff1a2f4e),
+        ),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment
+                .center, // Centra los contenedores horizontalmente
+            children: <Widget>[
+              // Primer contenedor con borde
+              Container(
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  color: const Color(0xfffff33e),
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(
+                    color: const Color(0xff000000),
+                    width: 1,
+                  ),
+                ),
+                child: const Center(),
+              ),
+              const SizedBox(
+                  width:
+                      25), // Espacio entre los contenedores (ahora es horizontal)
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+              // Segundo contenedor con sombra
+              Container(
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  color: const Color(0xffffffff),
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: const Color(0xff8b8b8b).withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset:
+                          const Offset(0, 3), // Cambia la posición de la sombra
+                    ),
+                  ],
+                ),
+                child: const Center(),
+              ),
+              const SizedBox(
+                  width:
+                      20), // Espacio entre los contenedores (ahora es horizontal)
+            ],
+          ),
         ),
       ),
     );
-  }
-}
+  } // fin widgets
+} // fin de la clase MiContenedorAPP
